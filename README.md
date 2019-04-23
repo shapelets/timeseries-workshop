4da46b3f9adc08070411bfddbee2bca9538588ad
Antonio Vilches <antonio.vilches@shapelets.io>
2019-04-23 12:04:14 +0200

    Readme notes and matrix style fix.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 In this workshop we give a spin on time series analytics. In particular, we avoid the traditional
 computation of statistics to focus on patterns, motifs, discords, shapelets and so on.
-The majority of this work has been developed and published by the research team of Professor Eamonn Keogh.
+Many of the algorithms and use cases used in this workshop has been first published by the research team 
+of Professor **Eamonn Keogh**.
--- matrixprofile/matrixprofile.py
+++ matrixprofile/matrixprofile.py
@@ -53,7 +53,7 @@ def sliding_dot_product_stomp(q, t):
 
     # Compute the inverse FFT to the element-wise multiplication of qraf and taf
     qt = np.fft.ifft(np.multiply(qraf, taf))
-    return qt[m-1:n]
+    return qt[m - 1:n]
 
 
 def calculate_distance_profile(q, t, qt, a, sum_q, sum_q2, mean_t, sigma_t):
@@ -99,14 +99,15 @@ def pre_compute_mean_std_for_TS_stomp(ta, m):
     # Compute the stats for t
     cumulative_sum_t = np.cumsum(ta)
     cumulative_sum_t2 = np.cumsum(np.power(ta, 2))
-    sum_t = (cumulative_sum_t[m-1:na] - np.concatenate(([0], cumulative_sum_t[0:na-m])))
-    sum_t2 = (cumulative_sum_t2[m-1:na] - np.concatenate(([0], cumulative_sum_t2[0:na-m])))
+    sum_t = (cumulative_sum_t[m - 1:na] - np.concatenate(([0], cumulative_sum_t[0:na - m])))
+    sum_t2 = (cumulative_sum_t2[m - 1:na] - np.concatenate(([0], cumulative_sum_t2[0:na - m])))
     mean_t = np.divide(sum_t, m)
     mean_t2 = np.divide(sum_t2, m)
     mean_t_p2 = np.power(mean_t, 2)
     sigma_t2 = np.subtract(mean_t2, mean_t_p2)
     sigma_t = np.sqrt(np.abs(sigma_t2))
     return sum_t, sum_t2, mean_t, mean_t2, mean_t_p2, sigma_t, sigma_t2
+
 
 # Mueen’s algorithim for similarity search (MASS)
 def mass(Q, T, a, meanT, sigmaT):
@@ -118,59 +119,61 @@ def mass(Q, T, a, meanT, sigmaT):
     return calculate_distance_profile(Q, T, QT, a, sumQ, sumQ2, meanT, sigmaT)
 
 
-def element_wise_min(Pab, Iab, D, idx, ignore_trivial, m):
+def element_wise_min(pab, Iab, D, idx, ignore_trivial, m):
     for i in range(0, len(D)):
-        #if it's a self-join, ignore trivial matches in [-m/2,m/2]
-        if not ignore_trivial or (np.abs(idx - i) > m/2.0):
-            if D[i] < Pab[i]:
-                Pab[i] = D[i]
+        # if it's a self-join, ignore trivial matches in [-m/2,m/2]
+        if not ignore_trivial or (np.abs(idx - i) > m / 2.0):
+            if D[i] < pab[i]:
+                pab[i] = D[i]
                 Iab[i] = idx
-    return Pab, Iab
+    return pab, Iab
 
 
-def stamp(Ta, Tb, m):
+def stamp(ta, tb, m):
     """
-    Compute the Matrix Profile between time-series Ta and Tb.
-    If Ta==Tb, the operation is a self-join and trivial matches are ignored.
+    Compute the Matrix Profile between time-series ta and tb.
+    If ta==tb, the operation is a self-join and trivial matches are ignored.
     
-    :param Ta: time-series, np.array
-    :param Tb: time-series, np.array
+    :param ta: time-series, np.array
+    :param tb: time-series, np.array
     :param m: subsequence length
     :return: Matrix Profile, Nearest-Neighbor indexes
     """
-    nb = len(Tb)
-    na = len(Ta)
-    Pab = np.ones(na - m)* np.inf
+    nb = len(tb)
+    na = len(ta)
+    pab = np.ones(na - m) * np.inf
     Iab = np.zeros(na - m)
     idxes = np.arange(nb - m + 1)
 
-    sumT, sumT2, meanT, meanT_2, meanTP2, sigmaT, sigmaT2 = pre_compute_mean_std_for_TS(Ta, m)
+    sumT, sumT2, meanT, meanT_2, meanTP2, sigmaT, sigmaT2 = pre_compute_mean_std_for_TS(ta, m)
 
     a = np.zeros(na - m)
     for i in range(0, na - m):
         a[i] = (sumT2[i] - 2 * sumT[i] * meanT[i] + m * meanTP2[i]) / sigmaT2[i]
 
-    ignore_trivial = np.atleast_1d(Ta == Tb).all()
+    ignore_trivial = np.atleast_1d(ta == tb).all()
     for idx in idxes:
-        D = mass(Tb[idx: idx + m], Ta, a, meanT, sigmaT)
-        if(ignore_trivial):
-            minIdx = int(np.maximum(idx-m/2.0,0))
-            maxIdx = int(np.minimum(idx+m/2.0,len(D)))
+        D = mass(tb[idx: idx + m], ta, a, meanT, sigmaT)
+        if (ignore_trivial):
+            minIdx = int(np.maximum(idx - m / 2.0, 0))
+            maxIdx = int(np.minimum(idx + m / 2.0, len(D)))
             D[minIdx:maxIdx:1] = np.inf
 
-        Iab[Pab>D] = i
-        Pab = np.minimum(Pab,D)
-    return Pab, Iab
+        Iab[pab > D] = i
+        pab = np.minimum(pab, D)
+    return pab, Iab
+
 
 def stampSJ(T, m):
     return stamp(T, T, m)
 
-def stomp(T, T2,  m):
+
+def stomp(T, T2, m):
     """
     Compute the Matrix Profile with self join for T
 
     :param T: time-series, np.array
-    :param Tb: time-series, np.array
+    :param tb: time-series, np.array
     :param m: subsequence length
     :return: Matrix Profile, Nearest-Neighbor indexes
     """
@@ -181,7 +184,7 @@ def stomp(T, T2,  m):
     seq_l = n - m
     _, _, meanT, _, _, sigmaT, _ = pre_compute_mean_std_for_TS_stomp(T, m)
 
-    Pab = np.full(seq_l + 1, np.inf)
+    pab = np.full(seq_l + 1, np.inf)
     Iab = np.zeros(n - m + 1)
     ignore_trivial = True
     for idx in range(0, seq_l):
@@ -198,15 +201,15 @@ def stomp(T, T2,  m):
         D[D < epsilon] = 0.0
         if (ignore_trivial):
             # ignore trivial minimum and  maximum
-            minIdx = int(np.maximum(idx - m / 2.0, 0))
-            maxIdx = int(np.minimum(idx + m / 2.0, len(D)))
-            D[minIdx:maxIdx:1] = np.inf
+            min_idx = int(np.maximum(idx - m / 2.0, 0))
+            max_idx = int(np.minimum(idx + m / 2.0, len(D)))
+            D[min_idx:max_idx:1] = np.inf
 
-        Iab[Pab > D] = idx
-        np.minimum(Pab, D, Pab)
+        Iab[pab > D] = idx
+        np.minimum(pab, D, pab)
 
-    np.sqrt(Pab, Pab)
-    return Pab, Iab
+    np.sqrt(pab, pab)
+    return pab, Iab
 
 
 def stompSJ(T, m):
@@ -214,7 +217,7 @@ def stompSJ(T, m):
     Compute the Matrix Profile with self join for T
 
     :param T: time-series, np.array
-    :param Tb: time-series, np.array
+    :param tb: time-series, np.array
     :param m: subsequence length
     :return: Matrix Profile, Nearest-Neighbor indexes
     """
@@ -230,36 +233,37 @@ def find_best_motif(profile):
 
 
 # Quick Test
-def test_stomp(Ta, m):
+def test_stomp(ta, m):
     start_time = time.time()
 
-    Pab, Iab = stomp(Ta, m)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    plot_motif(Ta, Pab, Iab, m)
-    return Pab, Iab
+    pab, iab = stomp(ta, m)
+    print("Execution time: " + str(time.time() - start_time))
+    plot_motif(ta, pab, iab, m)
+    return pab, iab
+
 
 # Quick Test
-def test_stamp(Ta, Tb, m):
+def test_stamp(ta, tb, m):
     start_time = time.time()
 
-    Pab, Iab = stamp(Ta, Tb, m)
+    pab, Iab = stamp(ta, tb, m)
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    plot_discord(Ta,  Pab, Iab, m, )
-    return Pab, Iab
+    plot_discord(ta, pab, Iab, m, )
+    return pab, Iab
 
 
-def plot_motif(Ta, values, indexes, m):
+def plot_motif(ta, values, m):
     plt.figure(figsize=(8, 4))
     plt.subplot(211)
-    plt.plot(Ta, linestyle='--', alpha=0.5)
-    plt.xlim((0, len(Ta)))
+    plt.plot(ta, linestyle='--', alpha=0.5)
+    plt.xlim((0, len(ta)))
 
     print(np.argmax(values))
 
-    plt.plot(range(np.argmin(values), np.argmin(values) + m), Ta[np.argmin(values):np.argmin(values) + m], c='g',
+    plt.plot(range(np.argmin(values), np.argmin(values) + m), ta[np.argmin(values):np.argmin(values) + m], c='g',
              label='Top Motif')
-    plt.plot(range(np.argmax(values), np.argmax(values) + m), Ta[np.argmax(values):np.argmax(values) + m], c='r',
+    plt.plot(range(np.argmax(values), np.argmax(values) + m), ta[np.argmax(values):np.argmax(values) + m], c='r',
              label='Top Discord')
 
     plt.legend(loc='best')
@@ -271,7 +275,7 @@ def plot_motif(Ta, values, indexes, m):
     plt.plot(np.argmax(values), np.max(values), marker='x', c='r', ms=10)
     plt.plot(np.argmin(values), np.min(values), marker='^', c='g', ms=10)
 
-    plt.xlim((0, len(Ta)))
+    plt.xlim((0, len(ta)))
     plt.xlabel('Index')
     plt.ylabel('Value')
     plt.tight_layout()
@@ -279,22 +283,23 @@ def plot_motif(Ta, values, indexes, m):
     plt.show()
 
 
-def plot_discord(Ta, Tb, values, indexes, m):
-    plt.figure(figsize=(8,4))
+def plot_discord(ta, tb, values, m):
+    plt.figure(figsize=(8, 4))
 
     plt.subplot()
-    plt.plot(Ta, linestyle='--')
-    plt.xlim((0, len(Ta)))
-    plt.plot(range(np.argmin(values), np.argmin(values) + m), Ta[np.argmin(values):np.argmin(values) + m], c='g', label='Best Match')
+    plt.plot(ta, linestyle='--')
+    plt.xlim((0, len(ta)))
+    plt.plot(range(np.argmin(values), np.argmin(values) + m), ta[np.argmin(values):np.argmin(values) + m], c='g',
+             label='Best Match')
     plt.legend(loc='best')
     plt.title('Time-Series')
-    plt.ylim((-3,3))
+    plt.ylim((-3, 3))
 
     plt.subplot()
-    plt.plot(Tb)
+    plt.plot(tb)
     plt.title('Query')
-    plt.xlim((0, len(Tb)))
-    plt.ylim((-3,3))
+    plt.xlim((0, len(tb)))
+    plt.ylim((-3, 3))
 
     plt.figure()
     plt.title('Matrix Profile')
@@ -302,7 +307,7 @@ def plot_discord(Ta, Tb, values, indexes, m):
     plt.plot(np.argmax(values), np.max(values), marker='x', c='r', ms=10)
     plt.plot(np.argmin(values), np.min(values), marker='^', c='g', ms=10)
 
-    plt.xlim((0, len(Ta)))
+    plt.xlim((0, len(ta)))
     plt.xlabel('Index')
     plt.ylabel('Value')
 
